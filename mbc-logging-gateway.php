129083a782afd2973ff5119aa95e5841b346af60
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-11 20:43:39 -0400

    Call correct class

--- mbc-logging-gateway.php
+++ mbc-logging-gateway.php
@@ -7,19 +7,20 @@
  */
 
 date_default_timezone_set('America/New_York');
+define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBC_ImportLogging\MBC_ImportLogging;
+use DoSomething\MBC_LoggingGateway\MBC_LoggingGateway;
 
 // Load configuration settings specific to this application
-require_once __DIR__ . '/mbc-import-logging.config.inc';
+require_once __DIR__ . '/mbc-logging-gateway.config.inc';
 
 
 echo '------- mbc-impoert-logging START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 // Kick off
 $mb = new MessageBroker($credentials, $config);
-$mb->consumeMessage(array(new MBC_ImportLogging($mb, $settings), 'updateLoggingAPI'));
+$mb->consumeMessage(array(new MBC_LoggingGateway($mb, $settings), 'consumeQueue'));
 
 echo '------- mbc-impoert-logging END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
