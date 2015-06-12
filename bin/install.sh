0d6413ca8997c2d46ed4f87aea04646d492b327d
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-12 16:15:01 -0400

    Script adjustments

--- bin/install.sh
+++ bin/install.sh
@@ -10,8 +10,7 @@ cd ../messagebroker-config
 MBCONFIG=`pwd`
 
 # Back to mbp-user-import
-cd ../mbp-user-import
+cd ../mbc-logging-gateway
 
 # Create SymLink for mbp-user-import application to make reference to for all Message Broker configuration settings
 ln -s $MBCONFIG .
-
