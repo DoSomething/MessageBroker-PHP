59cf09deabc87dbf523439640a1cedecc851d740
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-24 18:47:13 -0400

    Support interval and offset parms

--- mbc-logging-processor/mbc-logging-processor.php
+++ mbc-logging-processor/mbc-logging-processor.php
@@ -20,7 +20,25 @@ require_once __DIR__ . '/mbc-logging-gateway.config.inc';
 echo '------- mbc-logging-processor START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 // Kick off
-$mbcLoggingProcessor = new MBC_LoggingProcessor($credentials, $config, $settings);
-$mbcLoggingProcessor->processLoggedEvents();
+if (isset($_GET['interval'])) {
+  $interval = $_GET['interval'];
+}
+elseif (isset($argv[1])) {
+  $interval = $argv[1];
+}
+if (isset($_GET['offset'])) {
+  $offset = $_GET['offset'];
+}
+elseif (isset($argv[2])) {
+  $offset = $argv[2];
+}
+
+// Validate
+if (is_numeric($interval) && is_numeric($offset)) {
+
+  $mbcLoggingProcessor = new MBC_LoggingProcessor($credentials, $config, $settings);
+  $mbcLoggingProcessor->processLoggedEvents($interval, $offset);
+
+}
 
 echo '------- mbc-logging-processor END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
