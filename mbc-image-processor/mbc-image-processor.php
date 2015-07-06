42bca14ceeba1b9d896ddfb38b81d37e3031ddab
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-07-06 12:39:17 -0400

    Move config setting to inc file

--- mbc-image-processor/mbc-image-processor.php
+++ mbc-image-processor/mbc-image-processor.php
@@ -1,61 +1,22 @@
 <?php
 /**
- * mbc-a1_START_HERE.php
+ * mbc-image-processor
  *
- * Collect ?? from the ?? Queue. Process the entries to ??.
+ * Collect image details from the imageProcessingQueue. Use image path to make http request
+ * to trigger generation of image styles within Drupal application.
  */
 
 date_default_timezone_set('America/New_York');
+define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MB_Toolbox\MB_Configuration;
+use DoSomething\MBC_ImageProcessor\MBC_ImageProcessor;
 
-// Load configuration settings common to the Message Broker system
-// symlinks in the project directory point to the actual location of the files
-require_once __DIR__ . '/messagebroker-config/mb-secure-config.inc';
-require_once __DIR__ . '/MBC_Image_Processor.class.inc';
-
-// Settings
-$credentials = array(
-  'host' =>  getenv("RABBITMQ_HOST"),
-  'port' => getenv("RABBITMQ_PORT"),
-  'username' => getenv("RABBITMQ_USERNAME"),
-  'password' => getenv("RABBITMQ_PASSWORD"),
-  'vhost' => getenv("RABBITMQ_VHOST"),
-);
-
-$settings = array(
-  'stathat_ez_key' => getenv("STATHAT_EZKEY"),
-  'use_stathat_tracking' => getenv('USE_STAT_TRACKING'),
-  'ds_user_api_host' => getenv('DS_USER_API_HOST'),
-  'ds_user_api_port' => getenv('DS_USER_API_PORT'),
-);
-
-$config = array();
-$source = __DIR__ . '/messagebroker-config/mb_config.json';
-$mb_config = new MB_Configuration($source, $settings);
-$transactionalExchange = $mb_config->exchangeSettings('transactionalExchange');
-
-$config['exchange'] = array(
-  'name' => $transactionalExchange->name,
-  'type' => $transactionalExchange->type,
-  'passive' => $transactionalExchange->passive,
-  'durable' => $transactionalExchange->durable,
-  'auto_delete' => $transactionalExchange->auto_delete,
-);
-foreach ($transactionalExchange->queues->imageProcessingQueue->binding_patterns as $binding_key) {
-  $config['queue'][] = array(
-    'name' => $transactionalExchange->queues->imageProcessingQueue->name,
-    'passive' => $transactionalExchange->queues->imageProcessingQueue->passive,
-    'durable' =>  $transactionalExchange->queues->imageProcessingQueue->durable,
-    'exclusive' =>  $transactionalExchange->queues->imageProcessingQueue->exclusive,
-    'auto_delete' =>  $transactionalExchange->queues->imageProcessingQueue->auto_delete,
-    'bindingKey' => $binding_key,
-  );
-}
+// Load configuration settings specific to this application
+require_once __DIR__ . '/mbc-image-processor.config.inc';
 
 
 // Kick off - blocking, waiting for messages in the queue
 $mb = new MessageBroker($credentials, $config);
-$mb->consumeMessage(array(new MBC_Image_Processor($mb, $settings), 'consumeImageProcessingQueue'));
+$mb->consumeMessage(array(new MBC_ImageProcessor($mb, $settings), 'consumeImageProcessingQueue'));
