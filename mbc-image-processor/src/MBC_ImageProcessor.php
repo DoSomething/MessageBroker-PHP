ced4602054102aa8461488c6c50e304b5bc81073
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-07-08 18:39:29 -0400

    Stub MBC_ImageProcessor class

--- mbc-image-processor/src/MBC_ImageProcessor.php
+++ mbc-image-processor/src/MBC_ImageProcessor.php
@@ -1,70 +1,15 @@
 <?php
+namespace DoSomething\MBC_ImageProcessor;
+
+use DoSomething\StatHat\Client as StatHat;
+use DoSomething\MB_Toolbox\MB_Toolbox;
+
 /*
  * MBC_UserAPICampaignActivity.class.in: Used to process the transactionalQueue
  * entries that match the campaign.*.* binding.
  */
-
-use DoSomething\MB_Toolbox\MB_Toolbox;
-use DoSomething\MBStatTracker\StatHat;
-
 class MBC_ImageProcessor
 {
-
-  /**
-   * Message Broker connection to RabbitMQ
-   */
-  private $messageBroker;
-
-  /**
-   * Setting from external services - Mailchimp.
-   *
-   * @var array
-   */
-  private $settings;
-
-  /**
-   * Setting from external services - Mailchimp.
-   *
-   * @var array
-   */
-  private $statHat;
-
-  /**
-   * Constructor for MBC_TransactionalEmail
-   *
-   * @param array $settings
-   *   Settings from external services - StatHat
-   */
-  public function __construct($messageBroker, $settings) {
-
-    $this->messageBroker = $messageBroker;
-    $this->settings = $settings;
-
-    $this->toolbox = new MB_Toolbox($settings);
-    $this->statHat = new StatHat($settings['stathat_ez_key'], 'mbc-a1-startHere:');
-    $this->statHat->setIsProduction(isset($settings['use_stathat_tracking']) ? $settings['use_stathat_tracking'] : FALSE);
-  }
-
-  /**
-   * Initial method triggered by blocked call in mbc-image-processor.php. The $payload is the
-   * contents of the message being processed from the queue.
-   *
-   * @param array $payload
-   *   The contents of the queue entry
-   */
-  public function consumeImageProcessingQueue($payload) {
-    
-    echo '------- mbc-image-processor - consumeImageProcessingQueue() START -------', PHP_EOL;
-
-    $payloadDetails = unserialize($payload->body);
-    
-    // Only process queue entries that have image details
-    if ($payloadDetails['campaign_reportback'] && isset($payloadDetails['image_markup'])) {
-      $this->processImage($payloadDetails['image_markup']);
-    }
-    
-    echo '------- mbc-image-processor - consumeImageProcessingQueue() START -------', PHP_EOL;
-  }
 
   /**
    * Method to process image details. Make requests to trigger image cache processing on the Drupal site.
@@ -72,12 +17,7 @@ class MBC_ImageProcessor
    * @param array $payload
    *   The contents of the queue entry
    */
-  public function processImage($imageMarkup) {
-  
-    // Some processing of the message payload
-    $post = array(
-      'email' => '??',
-    );
+  public function process() {
 
     // An example cURL POST to the Message Broker User API (mb-user-api) to store peristant
     // Message Broker relivant data using the MB_Toolbox library.
@@ -86,8 +26,7 @@ class MBC_ImageProcessor
     if ($port != 0) {
       $curlUrl .= ":$port";
     }
-    $curlUrl .= '/user';
-    $result = $this->toolbox->curlPOST($curlUrl, $post);
+    $result = $this->toolbox->curlGETImage($curlUrl, $post);
 
     // Log consumer activity to StatHat for monitoring
     $this->statHat->clearAddedStatNames();
