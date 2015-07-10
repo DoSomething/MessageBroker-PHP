2ed9a235f7146c7861a7ad88c30791acb43532e6
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-07-10 16:21:05 -0400

    Add throttle method to base class

--- mbc-image-processor/mbc-image-processor.php
+++ mbc-image-processor/mbc-image-processor.php
@@ -27,7 +27,6 @@ $sh = new StatHat([
 ]);
 $tb = new MB_Toolbox($settings);
 
-
 // Kick off - block, waiting for messages in queue
 echo '------- mbc-logging-gateway START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 $mb->consumeMessage(array(new MBC_ImageProcessingConsumer($mb, $sh, $tb, $settings), 'consumeImageProcessingQueue'));
