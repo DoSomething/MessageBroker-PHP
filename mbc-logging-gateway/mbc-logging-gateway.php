334ba2c8af831e20b82dbbc8c0f6816b3563658d
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-12-22 18:47:31 -0500

    Add QOS setting to blocking call to support parallel processing

--- mbc-logging-gateway/mbc-logging-gateway.php
+++ mbc-logging-gateway/mbc-logging-gateway.php
@@ -8,6 +8,10 @@
 
 date_default_timezone_set('America/New_York');
 define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
+// The number of messages for the consumer to reserve with each callback
+// See consumeMwessage for further details.
+// Necessary for parallel processing when more than one consumer is running on the same queue.
+define('QOS_SIZE', 1);
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
@@ -21,6 +25,6 @@ echo '------- mbc-impoert-logging START - ' . date('j D M Y G:i:s T') . ' ------
 
 // Kick off
 $mb = new MessageBroker($credentials, $config);
-$mb->consumeMessage(array(new MBC_LoggingGateway($mb, $settings), 'consumeQueue'));
+$mb->consumeMessage(array(new MBC_LoggingGateway($mb, $settings), 'consumeQueue'), QOS_SIZE);
 
 echo '------- mbc-impoert-logging END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
