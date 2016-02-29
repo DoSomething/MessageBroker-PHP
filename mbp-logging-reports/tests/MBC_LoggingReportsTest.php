e525b797fc96793e337e13b2771b90c63065fc4a
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-02-29 10:52:23 -0500

    Testing stub file updated to be relavent to this app

--- mbp-logging-reports/tests/MBC_LoggingReportsTest.php
+++ mbp-logging-reports/tests/MBC_LoggingReportsTest.php
@@ -1,32 +1,33 @@
 <?php
+/**
+ *
+ */
 
-use DoSomething\MBC_LoggingGateway\MBC_LoggingGateway;
+namespace DoSomething\MBC_LoggingReports\Test;
 
-  // Including that file will also return the autoloader instance, so you can store
-  // the return value of the include call in a variable and add more namespaces.
-  // This can be useful for autoloading classes in a test suite, for example.
-  // https://getcomposer.org/doc/01-basic-usage.md
-  $loader = require_once __DIR__ . '/../vendor/autoload.php';
+/**
+ * MBP_LoggingReportsTest: test coverage for MBP_LoggingReports class.
+ */
+class MBP_LoggingReportsTest extends PHPUnit_Framework_TestCase {
  
 class MBC_LoggingProcessoryTest extends PHPUnit_Framework_TestCase {
   
   public function setUp(){ }
   public function tearDown(){ }
- 
-  public function testProcessLoggedEvents()
+  
+  /**
+   *
+   */
+  public function testDailyUserImport()
   {
-
+ 
     date_default_timezone_set('America/New_York');
 
-    // Load Message Broker settings used mb mbp-user-import.php
-    define('CONFIG_PATH',  __DIR__ . '/../messagebroker-config');
-    require_once __DIR__ . '/../mbc-logging-processor.config.inc';
+    // Load Message Broker settings used by mbp-logging-reports.php
+    require_once __DIR__ . '/../mbp-logging-reports.config.inc';
 
-    // Create  MBP_UserImport object to access findNextTargetFile() method for testing
-    $messageBroker = new MessageBroker($credentials, $config);
-    $mbcLoggingGateway = new MBC_LoggingProcessor($messageBroker, $settings);
-    
-    $this->assertTrue(TRUE);
+    // Assertion stub
+    $this->assertTrue(true);
+
   }
- 
 }
