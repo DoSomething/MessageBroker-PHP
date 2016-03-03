c2f5b36e0581acf2baf2ba8f35bfc8a00c123d93
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-03-02 22:01:58 -0500

    Add try/catch

--- mbp-logging-reports/mbp-logging-reports.php
+++ mbp-logging-reports/mbp-logging-reports.php
@@ -5,12 +5,14 @@
  * A producer to create reports based on logged data in mb-logging-api.
  */
 
+use DoSomething\MBP_LoggingReports\MBP_LoggingReports_Users;
+
 date_default_timezone_set('America/New_York');
 define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBP_LoggingReports\MBP_LoggingReports_Users;
+require_once __DIR__ . '/mbp-logging-reports.config.inc';
 
 if (isset($_GET['source'])) {
   $source = $_GET['source'];
@@ -24,10 +26,16 @@ else {
 
 echo '------- mbp-logging-reports START: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
 
-// Kick off
-$mbpLoggingReport = new MBP_LoggingReports_Users();
+try {
 
-// Gather digest message mailing list
-$mbpLoggingReport->report('nicheRunningMonth');
+  // Kick off
+  $mbpLoggingReport = new MBP_LoggingReports_Users();
+
+  // Gather digest message mailing list
+  $mbpLoggingReport->report('runningMonth', $source);
+}
+catch(Exception $e) {
+  echo $e->getMessage(), PHP_EOL;
+}
 
 echo '------- mbp-logging-reports END: ' . date('D M j G:i:s T Y') . ' -------', PHP_EOL;
