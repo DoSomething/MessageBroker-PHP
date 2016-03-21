e15fc80a70d58a51a4e82ae38b4594ed101598da
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-03-21 12:44:16 -0400

    Flush out command class structure for userImport

--- mbp-logging-reports/src/UserImportCommand.php
+++ mbp-logging-reports/src/UserImportCommand.php
@@ -11,4 +11,22 @@ use Symfony\Component\Console\Command\Command;
 class UserImportCommand extends Command
 {
 
+  /**
+   *
+   */
+  public function configure()
+  {
+    $this->setName('user-import')
+      ->setDescription('Report generation for user import activity.')
+      ->addArgument('source', InputArgument::REQUIRED, 'Import source: all, niche, afterschool');
+
+  }
+
+  /**
+   *
+   */
+  public function execute()
+  {
+
+  }
 }
