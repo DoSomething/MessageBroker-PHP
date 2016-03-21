4eea6f9e01c36c8fc13259a23963fe0a151700b8
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-03-21 12:52:37 -0400

    Restructure to use command class

--- mbp-logging-reports/src/UserImportCommand.php
+++ mbp-logging-reports/src/UserImportCommand.php
@@ -6,6 +6,9 @@
 namespace DoSomething\MBP_LoggingReports;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 
 class UserImportCommand extends Command
@@ -19,14 +22,14 @@ class UserImportCommand extends Command
     $this->setName('user-import')
       ->setDescription('Report generation for user import activity.')
       ->addArgument('source', InputArgument::REQUIRED, 'Import source: all, niche, afterschool');
-
   }
 
   /**
    *
    */
-  public function execute()
+  public function execute(InputInterface $input, OutputInterface $output)
   {
-
+    $source = $input->getArgument('source');
+    $output->writeln('------- mbp-logging-reports START: ' . date('D M j G:i:s T Y') . ' -------');
   }
 }
