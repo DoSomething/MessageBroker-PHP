4b7466c131da46c6bda472cadf972b3a2a81380d
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-04-07 22:36:29 -0400

    Move to master.blade

--- mb-monitor/resources/views/layout.blade.php
+++ mb-monitor/resources/views/layout.blade.php
@@ -3,8 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="/css/style.css">
   <title>Document</title>
+
   @yield('header')
+
 </head>
 <body>
   <div class="container">
