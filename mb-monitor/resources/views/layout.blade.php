d545b12a0ecdd822ab6e45b5d2fed6e1b3e1b29f
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-04-07 21:39:02 -0400

    Basic boiler plate

---
+++ mb-monitor/resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang=""en">
+<head>
+  <meta charset="UTF-8">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <title>Document</title>
+  @yield('header')
+</head>
+<body>
+  <div class="container">
+    <div class="content">
+
+      @yield('content')
+
+    </div>
+    <div class="footer">
+
+      @yield('footer')
+
+    </div>
+  </div>
+</body>
+</html>
