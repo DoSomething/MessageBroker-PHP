c24339292b84a7bd735911ca9c1659cf533d1924
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-05-16 13:24:19 -0400

    A few more rename tweeks

--- mbc-transactional-digest/mbc-transactional-digest.php
+++ mbc-transactional-digest/mbc-transactional-digest.php
@@ -11,16 +11,16 @@ define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\ MBC_TransactionalDigest\MBC_TransactionalDigest_Controller;
+use DoSomething\ MBC_TransactionalDigest\MBC_TransactionalDigest_Consumer;
 
 // Load configuration settings specific to this application
-require_once __DIR__ . '/mbc-logging-gateway.config.inc';
+require_once __DIR__ . '/mbc-transactional-digest.config.inc';
 
 
 // Kick off
 echo '------- mbc-transactional-digest START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 $mb = $mbConfig->getProperty('messageBroker');
-$mb->consumeMessage(array(new MBC_TransactionalDigest_Controller(), 'consumeTransactionalDigestQueue'));
+$mb->consumeMessage(array(new MBC_TransactionalDigest_Consumer(), 'consumeQueue'));
 
 echo '------- mbc-transactional-digest END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
