b12e6e6269214f063a5a1f8077c43ad258ce876b
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-05-16 12:53:15 -0400

    Clean up to reference correct names of classes and related methods

--- mbc-transactional-digest/mbc-transactional-digest.php
+++ mbc-transactional-digest/mbc-transactional-digest.php
@@ -2,28 +2,25 @@
 /**
  * mbc-transactional-digest
  *
- * Collect user .
+ * Collect transactional campaign sign up message requests in a certain time period and
+ * compose a single digest message request.
  */
 
 date_default_timezone_set('America/New_York');
 define('CONFIG_PATH',  __DIR__ . '/messagebroker-config');
-// The number of messages for the consumer to reserve with each callback
-// See consumeMwessage for further details.
-// Necessary for parallel processing when more than one consumer is running on the same queue.
-define('QOS_SIZE', 1);
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBC_LoggingGateway\MBC_LoggingGateway_Consumer;
+use DoSomething\ MBC_TransactionalDigest\MBC_TransactionalDigest_Controller;
 
 // Load configuration settings specific to this application
 require_once __DIR__ . '/mbc-logging-gateway.config.inc';
 
 
 // Kick off
-echo '------- mbc-impoert-logging START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
+echo '------- mbc-transactional-digest START - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 $mb = $mbConfig->getProperty('messageBroker');
-$mb->consumeMessage(array(new MBC_LoggingGateway_Consumer(), 'consumeLoggingGatewayQueue'), QOS_SIZE);
+$mb->consumeMessage(array(new MBC_TransactionalDigest_Controller(), 'consumeTransactionalDigestQueue'));
 
-echo '------- mbc-impoert-logging END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
+echo '------- mbc-transactional-digest END - ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
