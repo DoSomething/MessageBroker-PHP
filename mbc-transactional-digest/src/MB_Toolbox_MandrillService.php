c04d832d23f8f86033e225123aa511982cca5020
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-05-24 20:22:18 -0400

    Refactored to remove unused general digest functionality - make more transactional digest specific

--- mbc-transactional-digest/src/MB_Toolbox_MandrillService.php
+++ mbc-transactional-digest/src/MB_Toolbox_MandrillService.php
@@ -43,7 +43,7 @@ class MB_Toolbox_MandrillService extends MB_Toolbox_BaseService
     $this->transactionQueue = $this->mbConfig->getProperty('transactionalEmailQueue');
 
     $this->campaignMarkup = parent::getTemplate('campaign-markup.mandrill.inc');
-    $this->campaignTempateDivider = parent::getTemplate('campaign-divider-markup.mandrill.inc');
+    $this->campaignTemplateDivider = parent::getTemplate('campaign-divider-markup.mandrill.inc');
 
     $this->setGlobalMergeVars();
   }
@@ -110,71 +110,6 @@ class MB_Toolbox_MandrillService extends MB_Toolbox_BaseService
     return $campaignsMarkup;
  }
 
-  /**
-  * generateMessage(): Generate message values based on Mandrill Send-Template requirements.
-  *
-  * @param array $settings
-  *   Values to be used to generate message markup based on Mandrill API documentation:
-  *   Send-Template: https://mandrillapp.com/api/docs/messages.JSON.html#method=send-template
-  *   https://mandrillapp.com/api/docs/messages.JSON.html#method=send-template
-  *
-  *     - activity => 'campaign_signup_digest'
-  *     - email => 'xxx@yahoo.com'
-  *     - merge_vars =>
-  *       - INTRODUCTION
-  *       - CAMPAIGNS
-  *       - RECOMMENDATIONS
-  *     - user_language => 'en'
-  *     - email_template => 'mb-transactional-digest-v0-0-1'
-  *     - tags
-  *       - drupal_campaign_signup
-  *       - transactional_digest
-  *     - activity_timestamp
-  *     - application_id => US
-  *
-  *   Note: There's now support for "long SMS messages" of 2500 characters.
-  */
-  public function generateMessage($settings) {
-
-    $markup = 'MANDRILL MESSAGE';
-
-    return $markup;
-  }
-
-  /*
-   * getTransactionalDigestMessageSubject(): Generate the message subject text.
-   *
-   * @return string $subject
-   *   The dynamically generated message subject based on a list that
-   *   will change weekly.
-   */
-  private function getTransactionalDigestMessageSubject() {
-
-    $subjects = array(
-      'Your weekly DoSomething campaign digest',
-      'Your weekly DoSomething.org campaign roundup!',
-      'A weekly campaign digest just for you!',
-      'Your weekly campaign digest: ' . date('F j'),
-      date('F j') . ': Your weekly campaign digest!',
-      'Tips for your DoSomething.org campaigns!',
-      'Comin atcha: tips for your DoSomething.org campaign!',
-      '*|FNAME|* - Its your ' . date('F j') . ' campaign digest',
-      'Just for you: DoSomething.org campaign tips',
-      'Your weekly campaign tips from DoSomething.org',
-      date('F j') . ': campaign tips from DoSomething.org',
-      'You signed up for campaigns. Heres how to rock them!',
-      'Tips for you (and only you!)',
-      'Ready for your weekly campaign tips?',
-      'Your weekly campaign tips: comin atcha!',
-      'Fresh out the oven (just for you!)',
-    );
-    // Sequentially select an item from the list of subjects, a different one
-    // every week and start from the top once the end of the list is reached
-    $subjectCount = round((date('W') * count($subjects)) / 52);
-
-    return $subjects[$subjectCount];
-  }
-
   /*
    * An array of string to tag the message with. Stats are accumulated using
    * tags, though we only store the first 100 we see, so this should not be
@@ -183,63 +118,16 @@ class MB_Toolbox_MandrillService extends MB_Toolbox_BaseService
    * cause errors.
    *
    * @return array $tags
-   *   A list of tags to be associated with the digest messages.
+   *   A list of tags to be associated with the transactional digest messages.
    */
   private function getTransactionalDigestMessageTags() {
 
     $tags = array(
-      0 => 'digest',
+      0 => 'transactional',
+      1 => 'transactional-digest',
     );
 
     return $tags;
-  }
-
-  /*
-   * getTransactionalDigestMessageFrom(): Generate the message from name and email address.
-   *
-   * @return array $from
-   *   String values of the sender of the digest message.
-   */
-  private function getTransactionalDigestMessageFrom() {
-
-    $from = [
-      'email' => 'noreply@dosomething.org',
-      'name' => 'Ben, DoSomething.org'
-    ];
-
-    return $from;
-  }
-
-  /**
-   * getUsersTransactionalDigestSettings(): Generate "to" and "merge_var" values using the same index to ensure
-   * the indexes match.
-   *
-   * @return array $userDigestSettings
-   *   Formatted values based on Mandrill API requirements.
-   */
-  private function getTransactionalUsersDigestSettings() {
-
-    $messageIndex = 0;
-    $to = [];
-    $mergeVars = [];
-
-    if (!(isset($this->users)) || count($this->users) == 0) {
-      throw new Exception('getUsersTransactionalDigestSettings() $this->users not set.');
-    }
-    else {
-      foreach($this->users as $user) {
-        $to[$messageIndex] = $this->setTo($user);
-        $mergeVars[$messageIndex] = $this->getUserMergeVars($user);
-        $messageIndex++;
-      }
-
-      $userTransactionalDigestSettings = [
-        'to' => $to,
-        'merge_vars' => $mergeVars,
-      ];
-
-      return $userTransactionalDigestSettings;
-    }
   }
 
   /**
@@ -281,45 +169,65 @@ class MB_Toolbox_MandrillService extends MB_Toolbox_BaseService
     return $globalMergeVars;
   }
 
-  /*
-   * composeTransactionalDigestBatch(): Assemble all of the parts to create a sendTemplate submission to
-   * the Mandrill API.
+  /**
+   * getUserMergeVars():
    *
-   * @return array
-   *   All of the composed parts.
+   * @return array $mergeVars
+   *
    */
-  private function composeTransactionalDigestBatch() {
+  private function getUserMergeVars($campaignsMarkup, &$mergeVars) {
 
-    // subject line
-    $subject = $this->getTransactionalDigestMessageSubject();
+    $mergeVars['merge_vars']['CAMPAIGNS'] = $campaignsMarkup;
+  }
 
-    // from_email
-    // from_name
-    $from = $this->getDigestMessageFrom();
+ /**
+  * generateMessage(): Generate message values based on Mandrill Send-Template requirements.
+  *
+  * @param array $settings
+  *   Values to be used to generate message markup based on Mandrill API documentation:
+  *   Send-Template: https://mandrillapp.com/api/docs/messages.JSON.html#method=send-template
+  *   https://mandrillapp.com/api/docs/messages.JSON.html#method=send-template
+  *
+  *     - activity => 'campaign_signup_digest'
+  *     - email => 'xxx@yahoo.com'
+  *     - merge_vars =>
+  *       - INTRODUCTION
+  *       - CAMPAIGNS
+  *       - RECOMMENDATIONS
+  *     - user_language => 'en'
+  *     - email_template => 'mb-transactional-digest-v0-0-1'
+  *     - tags
+  *       - drupal_campaign_signup
+  *       - transactional_digest
+  *     - activity_timestamp
+  *     - application_id => US
+  *
+  *   Note: There's now support for "long SMS messages" of 2500 characters.
+  */
+  public function generateMessage($address, $campaignsMarkup) {
 
-    // Gather user settings in single request to ensure "to" and "marge_vars" are in sync
-    $usersDigestSettings = $this->getUsersTransactionalDigestSettings();
-    $to = $usersDigestSettings['to'];
-    $userMergeVars = $usersDigestSettings['merge_vars'];
+    $templateName = 'mb-transactional-digest-v0-0-1';
 
-    // global merge vars
-    $globalMergeVars = $this->getGlobalMergeVars();
+    // merge vars
+    $mergeVars = $this->getGlobalMergeVars();
+    $this->getUserMergeVars($campaignsMarkup, $mergeVars);
 
     // tags
     $tags = $this->getTransactionalDigestMessageTags();
 
-    $composedDigestSubmission = array(
-      'subject' => $subject,
-      'from_email' => $from['email'],
-      'from_name' => $from['name'],
-      'to' => $to,
-      'global_merge_vars' => $globalMergeVars,
-      'merge_vars' => $userMergeVars,
-      'tags' => $tags,
-    );
+    $message = [
+      'activity' => 'campaign_signup_digest',
+      'email' => $address,
+      'merge_vars' => $mergeVars,
+      'user_language' => 'en',
+      'email_tags' => $tags,
+      'activity_timestamp' => '',
+      'application_id' => ''
+    ];
 
-    return $composedDigestSubmission ;
+    return $message;
   }
+
 
  /**
   * dispatchMessages(): Send message to transactionalQueue to trigger sending transactional email messages.
@@ -327,25 +235,10 @@ class MB_Toolbox_MandrillService extends MB_Toolbox_BaseService
   * @param array $message
   *   Values to create message for processing in transactionalQueue.
   */
-  public function dispatchMessages($message) {
+  public function dispatchMessage($message) {
 
-    $templateName = '';
-    // Must be included in submission but is kept blank as the template contents
-    // are managed through the Mailchip/Mandril WYSIWYG interface.
-    $templateContent = array(
-      array(
-          'name' => 'main',
-          'content' => ''
-      ),
-    );
-
-    try {
-      $composedDigestBatch = $this->composeTransactionalDigestBatch();
- /// SEND TO transactionalQueue     $mandrillResults = $this->mandrill->messages->sendTemplate($templateName, $templateContent, $composedDigestBatch);
-    }
-    catch (Exception $e) {
-      echo '- MB_Toolbox_MandrillService->dispatchMessages(): Error sending composed transactional digest batch: ' . $e->getMessage(), PHP_EOL;
-    }
+    $message = serialize($payload);
+    $this->transactionQueue->publish($message, 'user.registration.transactional');
 
   }
 
