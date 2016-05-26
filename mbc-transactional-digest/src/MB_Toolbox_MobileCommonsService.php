a0d25f499518eacc18200028b86427b8d2c32215
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-05-26 18:46:39 -0400

    Impliment single and digest methods from base Service class - Mobile Commons

--- mbc-transactional-digest/src/MB_Toolbox_MobileCommonsService.php
+++ mbc-transactional-digest/src/MB_Toolbox_MobileCommonsService.php
@@ -104,7 +104,7 @@ class MB_Toolbox_MobileCommonsService extends MB_Toolbox_BaseService
   }
 
  /**
-  * generateMessage(): Generate message values based on Mobile Commons send_message() requirements.
+  * generateDigestMessage(): Generate message values based on Mobile Commons send_message() requirements.
   *
   * @param string $address
   *   The specific user address of the medium the service communicates with.
@@ -122,7 +122,7 @@ class MB_Toolbox_MobileCommonsService extends MB_Toolbox_BaseService
   * @return array $message
   *   Formatted message in format required by the service.
   */
-  public function generateMessage($address, $campaignsMarkup) {
+  public function generateDigestMessage($address, $campaignsMarkup) {
 
     $message['contents'] = 'MOBILE COMMONS MESSAGE';
 
@@ -136,7 +136,7 @@ class MB_Toolbox_MobileCommonsService extends MB_Toolbox_BaseService
   }
 
  /**
-  * dispatchMessage(): Send message to mobileCommonsQueue to trigger sending transactional Mobile Commons message.
+  * dispatchDigestMessage(): Send message to mobileCommonsQueue to trigger sending transactional Mobile Commons message.
   *
   *   Send SMS Message: https://secure.mcommons.com/api/send_message
   *   https://mobilecommons.zendesk.com/hc/en-us/articles/202052534-REST-API#SendSMSMessage.
@@ -144,50 +144,31 @@ class MB_Toolbox_MobileCommonsService extends MB_Toolbox_BaseService
   * @param array $message
   *   Values to create message for processing in transactionalQueue.
   */
-  public function dispatchMessage($message) {
+  public function dispatchDigestMessage($message) {
 
   }
 
+ /**
+  * generateSingleMessages():
+  *
+  * @param array $message
+  *   Values to create message for processing in transactionalQueue.
+  */
+  public function generateSingleMessage($address, $messageDetails) {
+
+    // $this->transactionQueue->publish($message, 'user.registration.transactional');
+  }
+
+ /**
+  * dispatchSingleMessages(): Send message to transactionalQueue to trigger sending transactional email message
+  * in signle campaign signup format.
+  *
+  * @param array $message
+  *   Values to create message for processing in transactionalQueue.
+  */
+  public function dispatchSingleMessage($payload) {
+
+    // $this->transactionQueue->publish($message, 'user.registration.transactional');
+  }
+
 }
-
-/*
-
-(
-    [activity] => campaign_signup
-    [email] => xxx@gmail.com
-    [uid] => 3404728
-    [merge_vars] => Array
-        (
-            [MEMBER_COUNT] => 5.3 million
-            [FNAME] => Jonice
-            [CAMPAIGN_TITLE] => World Recycle Week: Close The Loop 
-            [CAMPAIGN_LINK] => https://www.dosomething.org/us/campaigns/world-recycle-week-close-loop-0?source=node/362
-            [CALL_TO_ACTION] => Recycle old or worn-out clothes to help our planet.
-            [STEP_ONE] => Run Your Drive!
-            [STEP_TWO] => Snap a Pic
-            [STEP_THREE] => Drop It Off
-        )
-
-    [user_country] => US
-    [user_language] => en
-    [campaign_language] => en
-    [campaign_country] => US
-    [email_template] => mb-campaign-signup-US
-    [subscribed] => 1
-    [event_id] => 362
-    [email_tags] => Array
-        (
-            [0] => 362
-            [1] => drupal_campaign_signup
-        )
-
-    [mailchimp_list_id] => 8e7844f6dd
-    [mailchimp_grouping_id] => 10641
-    [mailchimp_group_name] => ComebackClothes2015
-    [mobile] => 1234567890
-    [mc_opt_in_path_id] => 203359
-    [activity_timestamp] => 1463500108
-    [application_id] => US
-)
-
-*/
