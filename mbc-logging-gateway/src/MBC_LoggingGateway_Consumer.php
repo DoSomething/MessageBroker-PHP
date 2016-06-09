726497cb82eed9d480305103cf7809de7d08ce03
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-06-09 17:04:49 -0400

    Only normalize some source values - removed support for specific source values on mb-logging-api

--- mbc-logging-gateway/src/MBC_LoggingGateway_Consumer.php
+++ mbc-logging-gateway/src/MBC_LoggingGateway_Consumer.php
@@ -405,7 +405,6 @@ class MBC_LoggingGateway_Consumer extends MB_Toolbox_BaseConsumer
     $cURLparameters['activity'] = $payloadDetails['activity'];
 
     $post = array();
-    $validSources = ['niche', 'hercampus', 'att-ichannel', 'teenlife', 'cgg', 'agg', 'us', 'ca', 'uk', 'gb', 'id', 'br', 'mx'];
 
     // Filter source to accepted values
     if ($payloadDetails['source'] == 'niche_mb_import') {
@@ -417,11 +416,8 @@ class MBC_LoggingGateway_Consumer extends MB_Toolbox_BaseConsumer
     elseif (strpos($payloadDetails['source'], 'node/') !== false) {
        $post['source'] = 'us';
     }
-    elseif (!in_array(strtolower($payloadDetails['source']), $validSources)) {
-      throw new Exception('Unsupported source: ' . $payloadDetails['source']);
-    }
     else {
-      $post['source'] = 'us';
+      $post['source'] = $payloadDetails['source'];
     }
 
     $post['activity_timestamp'] = $payloadDetails['activity_timestamp'];
