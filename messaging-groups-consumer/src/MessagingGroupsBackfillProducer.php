c5a085b62c37437c398bdd0a4f4981d0c2c8a02d
Sergii Tkachenko <hi@sergii.org>
2016-11-14 22:07:58 +0200

    Signups work.

--- messaging-groups-consumer/src/MessagingGroupsBackfillProducer.php
+++ messaging-groups-consumer/src/MessagingGroupsBackfillProducer.php
@@ -5,11 +5,15 @@
 
 namespace DoSomething\MessagingGroupsConsumer;
 
+// Native.
 use \Exception;
 
+// Contributed.
+use League\Csv\Reader;
+
+// DoSomething.
 use DoSomething\MB_Toolbox\MB_Configuration;
 use DoSomething\MB_Toolbox\MB_Toolbox_BaseProducer;
-
 
 class MessagingGroupsBackfillProducer extends MB_Toolbox_BaseProducer
 {
@@ -18,21 +22,40 @@ class MessagingGroupsBackfillProducer extends MB_Toolbox_BaseProducer
     echo '** mbp-user-import->processSignupCSV() '
         . ' START: ' . date('j D M Y G:i:s T') . ' -------' . PHP_EOL;
 
-    $data = [
-      'mobile' => '',
-      'application_id' => 'US',
-      'activity' => 'signup',
-      'event_id' => '',
-    ];
+    // $sql = "
+    //   SELECT mob.field_mobile_value AS mobile, c.entity_id AS campaign_id, dd.run_nid AS run_nid, dd.sid
+    //   FROM `dosomething_signup` AS `dd`
+    //   LEFT JOIN `field_data_field_mobile` AS mob ON mob.entity_id = dd.uid
+    //   LEFT JOIN `field_data_field_current_run` as c ON c.field_current_run_target_id = dd.run_nid
+    //   WHERE `run_nid` IN (7298, 7280, 7279, 7319, 7324) AND mob.field_mobile_value IS NOT NULL
+    // "
+    $csv = Reader::createFromPath('./data/ds-signups.csv');
 
-    $payload = $this->generatePayload($data);
-    parent->produceMessage($payload, 'messaging_groups_direct');
+    // Skip first row.
+    $data = $csv
+      ->setOffset(1)
+      ->setLimit(1)
+      ->fetchAssoc(["mobile","campaign_id","run_nid","sid"]);
 
+    foreach ($data as $row) {
+      $data = [
+        'mobile' => $row['mobile'],
+        'application_id' => 'US',
+        'activity' => 'campaign_signup',
+        'event_id' => $row['campaign_id'],
+      ];
+      echo '*** Sending ' . json_encode($row) . PHP_EOL;
+      $this->produceMessage(
+        $this->generatePayload($data),
+        'messaging_groups_direct'
+      );
+    }
   }
 
   public function processReportbackCSV() {
     echo '** mbp-user-import->processReportbackCSV() '
         . ' START: ' . date('j D M Y G:i:s T') . ' -------' . PHP_EOL;
   }
+
 
 }
